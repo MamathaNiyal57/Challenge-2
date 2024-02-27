4da97fc8b84412ef7a005ad9f892c99991ccae2b
Admin <admin@Admins-MacBook-Pro-25.local>
2024-02-27 10:12:12 +0530

    written base code

--- Trapezoid.java
+++ Trapezoid.java
@@ -1,1 +1,11 @@
-#Trapezoid area
+
+public class Trapezoid
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int height = sc.nextInt();
+    }
+}
